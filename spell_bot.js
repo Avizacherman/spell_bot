1432544287e49f084ced4c04e7cab103e1c4d775
Avi Zacherman <avizacherman@gmail.com>
2015-10-19 23:59:43 -0400

    error controls

--- spell_bot.js
+++ spell_bot.js
@@ -20,14 +20,16 @@ slack.on('message', function(message){
 		var channel = slack.getChannelGroupOrDMByID(message.channel)
 		spellNames = JSON.parse(body)
 		spellNames.forEach(function(spellName){
-				if(message.text.toLowerCase().match(spellName.name.toLowerCase())) {
-					request(`https://spell-buddy.herokuapp.com/api/spells/${spellName.id}`, function(err, response, body){
-						var spell = JSON.parse(body)[0]
-						var helper = new SpellHelpers(spell)
+				if(message.text){
+					if(message.text.toLowerCase().match(spellName.name.toLowerCase())) {
+						request(`https://spell-buddy.herokuapp.com/api/spells/${spellName.id}`, function(err, response, body){
+							var spell = JSON.parse(body)[0]
+							var helper = new SpellHelpers(spell)
 
-						channel.send(`\`\`\`${spell.name}\n Levels: ${spell.spell_level} \n School: ${spell.school} \t ${helper.subschool()} \t ${helper.descriptor()} \n Save: ${spell.saving_throw} \t Spell Resistance: ${spell.spell_resistence} \n Casting Time: ${spell.casting_time} \t Duration: ${spell.duration} Components: ${spell.components} \n Range: ${spell.range} \t ${helper.areaOrTargets()} \n \n ${spell.description} \`\`\``)
+							channel.send(`\`\`\`${spell.name}\n Levels: ${spell.spell_level} \n School: ${spell.school} \t ${helper.subschool()} \t ${helper.descriptor()} \n Save: ${spell.saving_throw} \t Spell Resistance: ${spell.spell_resistence} \n Casting Time: ${spell.casting_time} \t Duration: ${spell.duration} Components: ${spell.components} \n Range: ${spell.range} \t ${helper.areaOrTargets()} \n \n ${spell.description} \`\`\``)
 					})
 				}
+			}
 			
 		})
 	})
